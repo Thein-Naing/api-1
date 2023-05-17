ff090c33eeec6f59fea0a2d28a645aaa16e93530
Thein Naing <theinnaing0908@gmail.com>
2023-05-17 19:57:01 +0800

    inside useEffect hook, get request using axios.get('api address'),return a promise .then and in res block simply log res and .catch block simply log error

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
 function DataFetching() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=> {
+    axios.get("https://jsonplaceholder.typicode.com/posts").
+    then( res =>{
+      console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+    })
+  })
+
   return (
-    <div>DataFetching</div>
+    <div>
+
+    </div>
   )
-}
+
+
+
 
 export default DataFetching
